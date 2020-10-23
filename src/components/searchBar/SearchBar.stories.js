c18777cdc08bfcc0dc1ad8397f871e011b56776b
kryptondv <kryptondevpl@gmail.com>
2020-10-23 08:28:51 +0200

    add folders to stories

--- src/components/searchBar/SearchBar.stories.js
+++ src/components/searchBar/SearchBar.stories.js
@@ -2,9 +2,12 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 export default {
-  title: 'SearchBar',
+  title: 'header/SearchBar',
   component: SearchBar,
 };
 
 export const Light = () => <SearchBar variant="light"/>
 export const Dark = () => <SearchBar variant="dark"/>
+
+
+Dark.storyName = 'Dark Mode';
